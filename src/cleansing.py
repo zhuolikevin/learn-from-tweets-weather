c61ff5d3e735e3f4110107b6ef3d437e9c9b0e84
lizhuo <lizhuogo@gmail.com>
2017-04-14 20:36:42 -0500

    cleaned data

--- src/cleansing.py
+++ src/cleansing.py
@@ -11,6 +11,15 @@ def clean_data(input_file, output_file):
             for row in csv_reader:
                 text = row[1].strip().lower()
                 filtered_text = ''
+
+                # Remove `mention` and `link` info
+                mention_index = text.find('@mention')
+                if mention_index > -1:
+                    text = text[:mention_index] + text[mention_index + 8:]
+                link_index = text.find('{link}')
+                if link_index > -1:
+                    text = text[:link_index] + text[link_index + 6:]
+                
                 for i in range(len(text)):
                     if text[i] in ['(', ')']:
                         continue
@@ -19,7 +28,7 @@ def clean_data(input_file, output_file):
                     if text[i] in [',', '.', ':', ';'] and i < len(text) - 1 and text[i + 1] == ' ':
                         continue
                     filtered_text += text[i]
-                csv_writer.writerow([row[0], filtered_text] + row[2:])
+                csv_writer.writerow([row[0], filtered_text] + row[4:9] + row[13:])
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
