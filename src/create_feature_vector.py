91bd19e232bcdc64d2057953fad2bcdc591ec91c
zhout2 <zhout2@illinois.edu>
2017-04-15 22:05:17 -0500

    create feature vector

--- src/create_feature_vector.py
+++ src/create_feature_vector.py
@@ -1,4 +1,7 @@
 from sets import Set
+import nltk
+
+# put words into set to eliminate duplicates
 
 feature_set = set()
 
@@ -6,11 +9,37 @@ with open('../res/selected_words.txt') as fp:
     for line in fp:
         feature_set.add(line.split(',', 1)[0])
 
-print feature_set
-
+'''
 f = open("../res/cleaned_selected_words.txt", "w")
 
 for feature in feature_set:
     f.write(feature + '\n')
 
-f.close()
+f.close()'''
+
+# transform from set into dictionary
+
+count = 0
+feature_table = {}
+
+for feature in feature_set:
+    feature_table[feature] = count
+    count += 1
+
+# run through training text and create feature vector 
+
+with open('../res/cleaned_train.txt') as f_in:
+    f_out = open("../res/x_training.txt", "w")
+
+    for line in f_in:
+        vector = [0] * count
+
+        tokens = nltk.word_tokenize(line)  
+        for token in tokens:
+            index = feature_table.get(token)
+            if index != None:
+                vector[index] = 1
+
+        f_out.write(", ".join(map(str, vector)) + '\n')
+
+    f_out.close()
